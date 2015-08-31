1d7528149b5c134d8d0b3a88fa6d9f6a29f4aa51
Brian Downs <brian.downs@gmail.com>
2015-08-31 15:36:45 -0700

    removed commented and now unused code

--- config.go
+++ config.go
@@ -18,16 +18,6 @@ type Configuration struct {
 	API
 }
 
-// Configuration contains configuration.. duh
-/*
-type Configuration struct {
-	API struct {
-		Key    string `json:"key"`
-		Secret string `json:"secret"`
-	} `json:"api"`
-}
-*/
-
 // DigitalSignature creates an MD5 hash of the key, the secret and a timestamp
 func (c *Configuration) DigitalSignature() string {
 	now := time.Now()
@@ -49,20 +39,3 @@ func LoadConfig(key, secret string) *Configuration {
 		},
 	}
 }
-
-// LoadConfig builds a config obj
-/*
-func LoadConfig(cf string) (*Configuration, error) {
-	confFile, err := os.Open(cf)
-	if err != nil {
-		return nil, err
-	}
-	decoder := json.NewDecoder(confFile)
-	conf := &Configuration{}
-	err = decoder.Decode(conf)
-	if err != nil {
-		return nil, err
-	}
-	return conf, nil
-}
-*/
